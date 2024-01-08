2b78f66c56cdc626baabac06b5db7f277b9b057e
nono <nonoka.abe.24@gmail.com>
2024-01-08 22:52:42 +0900

    基本機能実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [inputBackgroundColor, setBackgroundColor] = useState('#0f0a95');
+  const [inputFontColor, setFontColor] = useState('#ffffff');
+  const [ButtonStyle, setButtonStyle] = useState({ backgroundColor: inputBackgroundColor, color: inputFontColor});
+
+  const setBackgroundColorValue = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+
+  const setFontColorValue = (e) => {
+    setFontColor(e.target.value);
+  };
+
+  const ColorChange = () => {
+    setButtonStyle({
+      backgroundColor: inputBackgroundColor,
+      color: inputFontColor,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p class = 'title'>配色確認ツール</p>
+        <div class='inputBox'>
+          <div>
+            <p>背景色を入力</p>
+            <input type = 'text' value={inputBackgroundColor} onChange={setBackgroundColorValue}/>
+          </div>
+          <div>
+            <p>文字色を入力</p>
+            <input type = 'text' value={inputFontColor} onChange={setFontColorValue}/>
+          </div>
+        </div>
+        <div class = 'changeButtonBox'>
+          <button onClick={ColorChange} class='changeButton'>色を変更</button>
+        </div>
+        <div class = 'sampleButtonBox'>
+          <button style={ButtonStyle} class = 'sampleButton'>サンプル</button>
+        </div>
     </div>
   );
 }
